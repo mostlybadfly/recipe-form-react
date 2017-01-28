e6b1a87bc2f90b61c76fc26a46f163c85617ba93
mostlybadfly <reachdanielnunez@gmail.com>
2017-01-28 16:59:11 -0600

    Add initial components

--- src/components/IngredientInput.js
+++ src/components/IngredientInput.js
@@ -1,11 +1,19 @@
 import React from 'react'
 
 class IngredientInput extends React.Component {
+  constructor(props) {
+    super(props); 
+    //testing to see if I can collect an array of ingredients
+    this.ingredients = []
+  }
+
   addIngredient(event) {
     if (event.key === 'Enter') {
+      this.ingredients.push(this.ingredientInput.value)
       event.preventDefault();
       // printing input value for testing purposes
       console.log(this.ingredientInput.value)
+      console.log (this.ingredients)
     }
   }
 
